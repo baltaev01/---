3e1cf32d124484f6f5bb4733faf5d8947c73f4f5
baltaev01 <ff4591997@gmail.com>
2025-09-19 18:04:39 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,75 @@
+# import random
+# generator urug' qiymatiga asoslangan tasodifiy sonni yaratadi, shuning uchun urug' qiymati 10 bo'lsa, siz har doim birinchi tasodifiy raqam sifatida 0,5714025946899135 ni olasiz.
+# random.seed(20)
+# print(random.random())
+#
+# import random
+# random.seed(45)
+# print(random.random())
+#
+# import random
+# x = random.getstate()
+# print(x)
+import random
+
+# import random
+#Tasodifiy generatorning joriy holatini qaytaring:
+# x = random.getstate()
+# print(x)
+#
+# #tasodifiy raqamni chop eting
+# print(random.random())
+# #davlatni qo'lga olish:
+# state = random.getstate()
+# #boshqa tasodifiy raqamni chop eting:
+# print(random.random())
+# #davlatni tiklash:\
+# random.setstate(state)
+# #va keyingi tasodifiy raqam siz davlatni qo'lga kiritganingiz bilan bir xil bo'lishi kerak:
+# print(random.random())
+#
+# import random
+# #tasodifiy raqamlar  chop etish
+# print(random.random())
+# #
+#
+# import random
+# # 8 bitli butun sonni qaytaring:
+# print(random.getrandbits(8))
+#
+# import random
+# #3 (qo'shilgan) va 9 (qo'shilmagan) orasidagi raqamni qaytaradi
+# print(random.randrange(3, 9))
+#
+# import random
+# # Ro'yxatdagi tasodifiy elementni qaytaring:
+# mylist = ["apple", "banana", "cherry"]
+# print(random.choice(mylist))
+#
+# import random
+# # 14 ta elementdan iborat roʻyxatni qaytaring.
+# # Ro'yxat belgilangan ro'yxatdagi qiymatlarning tasodifiy tanlanishini o'z ichiga olishi kerak va "olma" ni tanlash imkoniyati qolgan ikkitasiga qaraganda 10 baravar yuqori bo'lishi kerak:
+# mylist = ["apple", "banana", "cherry"]
+# print(random.choices(mylist, weights = [10, 1, 1], k = 14))
+#
+# import random
+#
+# mylist = ["apple", "banana", "cherry"]
+# random.shuffle(mylist)
+#
+# print(mylist)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
